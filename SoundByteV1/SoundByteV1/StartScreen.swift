ec80e5290a4e9b5d9d61a55b0adf4c6272a79500
Jack <jackdurfee@mines.edu>
2024-05-15 14:53:39 -0600

    Swift: Added start screen and timer to app

---
+++ SoundByteV1/SoundByteV1/StartScreen.swift
@@ -0,0 +1,18 @@
+//
+//  StartScree.swift
+//  SoundByteV1
+//
+//  Created by Jack Durfee on 5/15/24.
+//
+
+import SwiftUI
+
+struct StartScree: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StartScree()
+}
---
+++ SoundByteV1/SoundByteV1/Visual.swift
@@ -0,0 +1,18 @@
+//
+//  Visual.swift
+//  SoundByteV1
+//
+//  Created by Jack Durfee on 5/15/24.
+//
+
+import SwiftUI
+
+struct Visual: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Visual()
+}
