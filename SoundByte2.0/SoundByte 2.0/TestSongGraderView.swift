e76f4e1026ee8ca2d278024536c1af9a9c2be2a4
KieranTheYanaway <kyanaway@mines.edu>
2025-06-10 12:34:47 -0600

    Added TestSongGraderView - Kieran Yanaway kyanaway@mines.edu

--- SoundByte2.0/SoundByte 2.0/TestSongGraderView.swift
+++ SoundByte2.0/SoundByte 2.0/TestSongGraderView.swift
@@ -7,12 +7,56 @@
 
 import SwiftUI
 
+
+
+
 struct TestSongGraderView: View {
+    var filename: String
+    var songGrader: SongGrader
+    
+    
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            HStack {
+                Text(songGrader.intervalNotes[0].note)
+                Text(String(songGrader.intervalNotes[0].octave))
+                Text(String(songGrader.targetPitches[0]))
+            }
+            HStack {
+                Text(songGrader.intervalNotes[1].note)
+                Text(String(songGrader.intervalNotes[1].octave))
+                Text(String(songGrader.targetPitches[1]))
+            }
+            HStack {
+                Text(songGrader.intervalNotes[2].note)
+                Text(String(songGrader.intervalNotes[2].octave))
+                Text(String(songGrader.targetPitches[2]))
+            }
+            HStack {
+                Text(songGrader.intervalNotes[3].note)
+                Text(String(songGrader.intervalNotes[3].octave))
+                Text(String(songGrader.targetPitches[3]))
+            }
+            Text(String(songGrader.currentGrade))
+            Text(String(songGrader.sumCorrectPitches))
+            Text(String(songGrader.sumTotalPitches))
+        }
+    }
+    
+    init(filename: String) {
+        self.filename = filename
+        songGrader = SongGrader()
+        songGrader.loadNotes(filename: filename)
+        songGrader.calculateTargetPitches(targetNotes: songGrader.intervalNotes)
+        for i in 0..<songGrader.targetPitches.count {
+            songGrader.updateGrading(currentPitch: songGrader.targetPitches[i])
+            //songGrader.updateGrading(currentPitch: 200000.0)
+        }
     }
 }
 
 #Preview {
-    TestSongGraderView()
+    
+    TestSongGraderView(filename: "test-2")
 }
